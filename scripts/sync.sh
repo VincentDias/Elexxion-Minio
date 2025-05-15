395cf5481b5cb6aa78d2f61e8d71c848adbb2ccd
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-05-15 19:46:44 +0200

    maj

---
+++ scripts/sync.sh
@@ -0,0 +1,14 @@
+mc alias set minio-local http://minio:9000   $MINIO_ROOT_USER   $MINIO_ROOT_PASSWORD
+mc alias set aws-s3      https://s3.eu-west-1.amazonaws.com   $AWS_ACCESS_KEY_ID   $AWS_SECRET_ACCESS_KEY
+
+while true; do
+  echo "[`date +'%Y-%m-%d %H:%M:%S'`] Début de la sync..."
+  mc mirror \
+    --overwrite \
+    --remove \
+    --region eu-west-1 \
+    minio-local/elexxion-elt/ \
+    aws-s3/elexxion-minio-bucket/
+  echo "[`date +'%Y-%m-%d %H:%M:%S'`] Pause 60 s..."
+  sleep 60
+done
