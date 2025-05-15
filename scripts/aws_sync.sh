4587aa9e792e13b4ffbaa2ca0d4a52d5a6e4402e
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-05-15 20:45:59 +0200

    ++

--- scripts/aws_sync.sh
+++ scripts/aws_sync.sh
@@ -1,8 +1,8 @@
 mc alias set minio-local http://minio:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
-mc alias set aws-s3      https://s3.eu-west-1.amazonaws.com $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
+mc alias set aws-s3 https://s3.eu-west-1.amazonaws.com $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
 
 while true; do
-  echo "[`date +'%Y-%m-%d %H:%M:%S'`] Début de la sync..."
+  echo "[`date +'%Y-%m-%d %H:%M:%S'`] Début synchronisation..."
   mc mirror \
     --overwrite \
     --remove \
