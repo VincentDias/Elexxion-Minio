2b1ddb1f981a9eb3695abe45143858faa650c20b
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-05-19 15:53:31 +0200

    maj

--- scripts/aws_sync.sh
+++ scripts/aws_sync.sh
@@ -3,15 +3,17 @@ mc alias set aws-s3 https://s3.eu-west-1.amazonaws.com $AWS_ACCESS_KEY_ID $AWS_S
 
 while true; do
   echo "[`date +'%Y-%m-%d %H:%M:%S'`] Début synchronisation..."
+
   mc mirror \
     --overwrite \
     --remove \
     --region eu-west-1 \
     --watch \
-    minio-local/elexxion-minio-bucket/ \
-    aws-s3/elexxion-aws-bucket/ | while read line; do
+    minio-local/elexxion-minio-bucket \
+    aws-s3/elexxion-aws-bucket | while read line; do
       echo "✈️​ [`date +'%Y-%m-%d %H:%M:%S'`] $line"
   done
+
   echo "[`date +'%Y-%m-%d %H:%M:%S'`] Pause 60 s..."
   sleep 60
 done
